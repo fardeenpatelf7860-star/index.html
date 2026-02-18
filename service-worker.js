09844c637bf68b5930b8409f702c6e9ac70c8e28
fardeenpatelf7860-star <fardeenpatelf7860@gmail.com>
2026-02-19 02:54:43 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = "props-calculator-v1";
+const CACHE_NAME = "props-calculator-v3";
+
 const urlsToCache = [
-  "./",
-  "index.html",
   "logo.png",
   "manifest.json"
 ];
 
+// Install
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => caches.delete(cache))
+      );
+    })
   );
+  self.clients.claim();
+});
+
+// Fetch (always get fresh HTML)
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request));
+  }
 });
